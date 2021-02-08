992cefe945159aa087a2d5fb4e95b0c61ac9ad50
Ben Tuttle <bentuttle2000@gmail.com>
2021-02-08 18:05:50 -0500

    edit welcome file ben
    added to welcome file

--- Assets/Scripts/welcome.cs
+++ Assets/Scripts/welcome.cs
@@ -7,6 +7,8 @@ public class welcome : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //this is a test welcome file to see how working together in unity will work
+        //please add a print("your_name"); line then save and push to your branch and post in the discord saying you have done this
         print("Welcome");
         print("Ben");
     }
