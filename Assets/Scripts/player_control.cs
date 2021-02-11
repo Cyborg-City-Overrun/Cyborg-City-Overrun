1d75b4c7b26a16010783299356a6d283b9d3e255
Ben Tuttle <bentuttle2000@gmail.com>
2021-02-10 22:32:07 -0500

    Added a basic player with basic movement
    drew an image for a player animation (just a circle for now) and added a script to make it move and animate

---
+++ Assets/Scripts/player_control.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_control : MonoBehaviour
+{
+    // Variables
+    private Rigidbody2D myRigidBody;
+
+    private Animator myAnim;
+
+    private float mySpeed = 4f;
+
+    private Vector2 myMovement = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myMovement.x = Input.GetAxisRaw("Horizontal");
+        myMovement.y = Input.GetAxisRaw("Vertical");
+        UpdateAnimation();
+        Move();
+    }
+
+    void UpdateAnimation()
+    {
+        if (myMovement != Vector2.zero)
+        {
+            myAnim.SetFloat("moveX", myMovement.x);
+            myAnim.SetFloat("moveY", myMovement.y);
+            myAnim.SetBool("moving", true);
+        }
+        else
+        {
+            myAnim.SetBool("moving", false);
+        }
+    }
+
+    void Move()
+    {
+        myRigidBody.MovePosition(myRigidBody.position + myMovement * mySpeed * Time.fixedDeltaTime);
+    }
+}
