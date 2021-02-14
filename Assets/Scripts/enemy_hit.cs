fa66199c057fd7b9ea2977e63ac73dfa0117d1b8
Ben Tuttle <bentuttle2000@gmail.com>
2021-02-13 22:59:40 -0500

    made enemy lose health when its hit
    added collision effects with enemy so player can damage it

---
+++ Assets/Scripts/enemy_hit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_hit : MonoBehaviour
+{
+    private float myHealth = 20f;
+
+    private void OnTriggerEnter2D(Collider2D hit)
+    {
+        if (hit.gameObject.tag == "Attack")
+        {
+            myHealth -= 5;
+            print(myHealth);
+            if (myHealth <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
