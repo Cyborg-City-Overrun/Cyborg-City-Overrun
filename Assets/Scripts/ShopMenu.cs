8e1340e4278c22f911dbfeeecdd1c65584a3e186
Ben Tuttle <bentuttle2000@gmail.com>
2021-02-16 15:44:51 -0500

    added a shop
    added a shop (press E while nearby to open menu) and a health potion (restores 25 health for 10 money) player starts with 100 money

---
+++ Assets/Scripts/ShopMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopMenu : MonoBehaviour
+{
+    private GameObject myPlayer;
+
+    public void Start()
+    {
+        myPlayer = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void HealthPotion()
+    {
+        print("Health Potion: 10");
+        if (myPlayer.GetComponent<player_control>().money >= 10)
+        {
+            myPlayer.GetComponent<player_control>().money -= 10;
+            print("Purchased");
+            print(myPlayer.GetComponent<player_control>().money);
+            myPlayer.GetComponent<player_control>().RestoreHealth(25);
+        }
+        else
+        {
+            print("Not enough money");
+        }
+    }
+}
