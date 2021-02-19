7e78dae806fad06ebef8add7d9485a58278df8c9
Timothy <timothy.tu526@gmail.com>
2021-02-19 01:37:16 -0500

    Moved PauseMenu & DeathMenu into Game Scene

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -5,9 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class OptionsMenu : MonoBehaviour
 {
-
+    public Canvas PauseMenu;
     public void Restart()
     {
         SceneManager.LoadScene(0);
+    }
+    public void HideMe()
+    {
+        PauseMenu.gameObject.SetActive(false);
     }
 }
