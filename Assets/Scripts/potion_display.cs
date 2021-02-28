30c6b7246a34e4849df7373a4177449b7310930b
Ben Tuttle <bentuttle2000@gmail.com>
2021-02-27 19:51:43 -0500

    updated potion display

--- Assets/Scripts/potion_display.cs
+++ Assets/Scripts/potion_display.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class potion_display : MonoBehaviour
 {
+    Text text;
+    public GameObject potionToDisplay;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        text = GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        text.text = potionToDisplay.GetComponent<potions>().getNumberOfPotions().ToString();
     }
 }
