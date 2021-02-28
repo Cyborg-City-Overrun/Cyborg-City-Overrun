670019b92397748f90c15617b3c84c6dfa8be823
Ben Tuttle <bentuttle2000@gmail.com>
2021-02-27 20:45:17 -0500

    updated menu prices and item drops
    made prices accurate and made drops picked up by pressing E

--- Assets/Scripts/heart_item.cs
+++ Assets/Scripts/heart_item.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class heart_item : item
 {
-    
+    private GameObject myPlayer;
+
+    private void Start()
+    {
+        myPlayer = GameObject.FindGameObjectWithTag("Player");
+    }
 
     private void OnTriggerEnter2D(Collider2D hit)
     {
-        if (hit.gameObject.tag == "Player")
+        if (hit.gameObject.tag == "Interact")
         {
-            hit.GetComponent<player_control>().RestoreHealth(this.restoreAmount);
+            myPlayer.GetComponent<player_control>().RestoreHealth(this.restoreAmount);
             Destroy(gameObject);
         }
     }
