8f2517ddda9919883333f7e032286da001aeb008
Ben Tuttle <bentuttle2000@gmail.com>
2021-03-02 22:08:46 -0500

    update to menu

---
+++ Assets/Scripts/display_inventory.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class display_inventory : MonoBehaviour
+{
+    public GameObject[] slots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Display_Items()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            //slots[i].transform.GetChild(0).GetComponent
+        }
+    }
+}
