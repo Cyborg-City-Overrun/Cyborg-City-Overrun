b7571d8bee3336e8c711416b6b9285936a63b39e
Stephanie <xxblossomingonionxx@gmail.com>
2021-03-15 17:40:36 -0400

    Imported yaSingleton to work in layers

---
+++ Assets/Scripts/Sorter.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using yaSingleton;
+
+public class Sorter : Singleton<SpriteRenderOrderSystem>
+{
+    void onUpdate()
+    {
+        //if(transform.position > )
+    }
+}
