9a7732e5376e921a71bd35a0063ef83bcd726d87
Stephanie <xxblossomingonionxx@gmail.com>
2021-03-21 18:35:30 -0400

    further vending machine updates

--- Assets/Scripts/VendingMachine.cs
+++ Assets/Scripts/VendingMachine.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class VendingMachine : MonoBehaviour
 {
@@ -10,10 +12,16 @@ public class VendingMachine : MonoBehaviour
     public int price;
 
     public GameObject player;
-    public potions vendPotion = new potions();
+    public potions vendHealth = new potions();
+    public potions vendEnergy = new potions();
+    public potions vendAttack = new potions();
+
+
     void Start()
     {
-        
+        vendHealth.myID = 0;
+        vendEnergy.myID = 1;
+        vendAttack.myID = 2;
     }
 
     // Update is called once per frame
@@ -33,28 +41,19 @@ public class VendingMachine : MonoBehaviour
                 switch (lootIndex)
                 {
                     case 0:
-                        vendPotion.myID = 0;
-                        price = vendPotion.getPrice();
-                        if (player.GetComponent<player_control>().Transaction(-price))
-                        {
-                            Instantiate(Loot[lootIndex].gameObject, new Vector3(this.transform.position.x + Random.Range(-1.0f, 1.0f), this.transform.position.y - Random.Range(.5f, 1.0f), -1), Quaternion.identity);
-                        }
+                        price = vendHealth.getPrice();
+                        player.GetComponent<player_control>().Transaction(-price);
+                        Instantiate(Loot[lootIndex].gameObject, new Vector3(this.transform.position.x + Random.Range(-1.0f, 1.0f), this.transform.position.y - Random.Range(.5f, 1.0f), -1), Quaternion.identity);
                         break;
                     case 1:
-                        vendPotion.myID = 1;
-                        price = vendPotion.getPrice();
-                        if (player.GetComponent<player_control>().Transaction(-price))
-                        {
-                            Instantiate(Loot[lootIndex].gameObject, new Vector3(this.transform.position.x + Random.Range(-1.0f, 1.0f), this.transform.position.y - Random.Range(.5f, 1.0f), -1), Quaternion.identity);
-                        }
+                        price = vendEnergy.getPrice();
+                        player.GetComponent<player_control>().Transaction(-price);
+                        Instantiate(Loot[lootIndex].gameObject, new Vector3(this.transform.position.x + Random.Range(-1.0f, 1.0f), this.transform.position.y - Random.Range(.5f, 1.0f), -1), Quaternion.identity);
                         break;
                     case 2:
-                        vendPotion.myID = 2;
-                        price = vendPotion.getPrice();
-                        if (player.GetComponent<player_control>().Transaction(-price))
-                        {
-                            Instantiate(Loot[lootIndex].gameObject, new Vector3(this.transform.position.x + Random.Range(-1.0f, 1.0f), this.transform.position.y - Random.Range(.5f, 1.0f), -1), Quaternion.identity);
-                        }
+                        price = vendAttack.getPrice();
+                        player.GetComponent<player_control>().Transaction(-price);
+                        Instantiate(Loot[lootIndex].gameObject, new Vector3(this.transform.position.x + Random.Range(-1.0f, 1.0f), this.transform.position.y - Random.Range(.5f, 1.0f), -1), Quaternion.identity);
                         break;
 
                 }
