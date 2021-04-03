75343174bda09024f1c64620d48805ee2dd7f184
Daura951 <murphyd6@sunypoly.edu>
2021-04-02 22:33:10 -0400

    Fixed glitch with shop keeper dialogue

--- Assets/Scripts/Shop Scripts/shopKeeper.cs
+++ Assets/Scripts/Shop Scripts/shopKeeper.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class shopKeeper : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class shopKeeper : MonoBehaviour
     public dialogueParser[] randTalk;
     private GameObject player;
     private bool firstInteracted = false;
+    private int[] randIndex = { 0, 1, 2 };
+    private int totalClick = 0;
+    public Image talkButtonImage;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,16 +28,52 @@ public class shopKeeper : MonoBehaviour
 
     public bool doInteract(bool isItFirst)
     {
+        totalClick++;
+        bool isRandPicked = false;
         bool firstInteract = isItFirst;
-        if (firstInteract == false)
+        if(totalClick<=4)
         {
-            firstInteraction.setIsInteracted(true);
+            if (firstInteract == false)
+            {
+                firstInteraction.setIsInteracted(true);
+            }
+            else
+            {
+                while (!isRandPicked)
+                {
+                    int totalNeg = 0;
+                    foreach (int num in randIndex)
+                    {
+                        if (num == -1)
+                        {
+                            totalNeg++;
+                        }
+                    }
+
+                    if (totalNeg > 3)
+                    {
+                        isRandPicked = true;
+                        print(":o");
+                    }
+
+                    else
+                    {
+                        int rand = (int)(Random.Range(0, 3));
+                        if (randIndex[rand] != -1)
+                        {
+                            randTalk[rand].setIsInteracted(true);
+                            randIndex[rand] = -1;
+                            isRandPicked = true;
+                        }
+                    }
+
+                }
+            }
         }
         else
         {
-            randTalk[(int)(Random.Range(0, 5))].setIsInteracted(true);
+            talkButtonImage.color = new Color(0.18f, 0.16f, 0.25f,1);
         }
-
         return true;
     }
 
