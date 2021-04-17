b5aa309e8493cdb5eb7b615354ad1f63db1706a0
Ben Tuttle <bentuttle2000@gmail.com>
2021-04-17 17:25:04 -0400

    added upgrade drops to chests

--- Assets/Scripts/Item Scripts/upgradeDrop.cs
+++ Assets/Scripts/Item Scripts/upgradeDrop.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class upgradeDrop : MonoBehaviour
+public class upgradeDrop : item
 {
     public enum types { red, green, yellow };
     public types type;
