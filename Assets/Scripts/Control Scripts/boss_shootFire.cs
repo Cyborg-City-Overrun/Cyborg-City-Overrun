4e29c7ab4dd75183cccee01d1522a958dfd6c041
Stephanie <xxblossomingonionxx@gmail.com>
2021-04-25 01:39:59 -0400

    Attempted boss attack

---
+++ Assets/Scripts/Control Scripts/boss_shootFire.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boss_shootFire : MonoBehaviour
+{
+
+    private Animator myAnim;
+    private Rigidbody2D myBody;
+    public GameObject boss;
+    public GameObject funnyFire;
+    private bool flaming = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBody = gameObject.GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
+
+
+    }
+
+    // Update is called once per frame
+    void startFlame()
+    {
+        if (flaming == false)
+        {
+            flaming = true;
+            while (boss.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+            {
+                if (boss.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsTag("Attack") == true && boss.GetComponent<Animator>().GetFloat("moveX") == 1)
+                {
+                    Instantiate(funnyFire.gameObject, new Vector3(boss.GetComponent<GameObject>().transform.GetChild(3).position.x, boss.GetComponent<GameObject>().transform.GetChild(3).position.y, -1), Quaternion.identity);
+                    funnyFire.GetComponent<Animator>().Play("Right");
+                }
+                else if (boss.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsTag("Attack") == true && boss.GetComponent<Animator>().GetFloat("moveX") == -1)
+                {
+                    Instantiate(funnyFire.gameObject, new Vector3(boss.GetComponent<GameObject>().transform.GetChild(3).position.x, boss.GetComponent<GameObject>().transform.GetChild(3).position.y, -1), Quaternion.identity);
+                    funnyFire.GetComponent<Animator>().Play("Left");
+                }
+                else if (boss.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsTag("Attack") == true && boss.GetComponent<Animator>().GetFloat("moveY") == 1)
+                {
+                    Instantiate(funnyFire.gameObject, new Vector3(boss.GetComponent<GameObject>().transform.GetChild(3).position.x, boss.GetComponent<GameObject>().transform.GetChild(3).position.y, -1), Quaternion.identity);
+                    funnyFire.GetComponent<Animator>().Play("Up");
+                }
+                else if (boss.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsTag("Attack") == true && boss.GetComponent<Animator>().GetFloat("moveY") == -1)
+                {
+                    Instantiate(funnyFire.gameObject, new Vector3(boss.GetComponent<GameObject>().transform.GetChild(3).position.x, boss.GetComponent<GameObject>().transform.GetChild(3).position.y, -1), Quaternion.identity);
+                    funnyFire.GetComponent<Animator>().Play("Down");
+                }
+                else
+                {
+
+                }
+            }
+            flaming = false;
+        }
+        else
+        {
+
+        }
+    }
+}
